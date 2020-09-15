ee5cf5dbfaae96ce0b14817b63c368d4a2472563
Conner Starkel <csmediaseattle@gmail.com>
2020-09-14 20:42:19 -0700

    Add project files.

---
+++ Crob/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crowsound(object sender, EventArgs e)
+        {
+            System.IO.Stream str = Crob.Properties.Resources.crow;
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+            snd.Play();
+        }
+    }
+}
---
+++ Crob/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nash
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form1 morenash = new Form1();
+            int x;
+            int y;
+            Random rn = new Random(Guid.NewGuid().GetHashCode());
+            Random ra = new Random(Guid.NewGuid().GetHashCode());
+            Rectangle r = Screen.PrimaryScreen.WorkingArea;
+            x = rn.Next(0, Screen.PrimaryScreen.WorkingArea.Width);
+            y = ra.Next(0, Screen.PrimaryScreen.WorkingArea.Height);
+            morenash.StartPosition = FormStartPosition.Manual;
+            morenash.Location = new Point(x, y);
+            morenash.Show();
+        }
+    }
+}
